5014ea2763692de4d501d74fb391256dc31331b5
Alexey <Alexey_Rudych@ukr.net>
2016-10-27 16:16:39 +0300

    frelancer

---
+++ app/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+	$('.menuIcon').click(function(){
+		$('.mobileMenu').slideToggle("slow");
+	})
+//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("form").submit(function() {
+		$.ajax({
+			type: "GET",
+			url: "mail.php",
+			data: $("form").serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.fancybox.close();
+			}, 1000);
+		});
+		return false;
+	});
+});
